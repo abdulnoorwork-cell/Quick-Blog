bdb13bffb70415e840c04e268b7ae8b8ab0b3bfd
abdulnoor <abdulnoorwork@gmail.com>
2026-02-20 12:08:12 +0500

    save

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -27,11 +27,11 @@ const BlogList = () => {
                     </div>
                 ))}
             </div>
-            {blogs ? <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24'>
                 {filterBlogs().filter(blog => menu === 'All' ? true : menu === blog.category).map((blog, index) => (
                     <BlogCard key={index} blog={blog} />
                 ))}
-            </div> : <div>Loading...</div>}
+            </div>
         </div>
     )
 }
